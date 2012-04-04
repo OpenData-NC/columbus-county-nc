c261c9ae01890280d58725c08ece7af25a79f681
Colin Copeland <copelco@caktusgroup.com>
2012-04-04 08:31:51 -0400

    begin adding base scraper and models

--- openrural/data_dashboard/models.py
+++ openrural/data_dashboard/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-
-# class Scraper(models.Model):
-#     name = models.CharField(max_length=255)
+from ebpub.db.models import Schema
 
 
-# class Run(models.Model):
-#     scraper = models.ForeignKey(Scraper)
-#     date = models.DateTimeField(auto_now_add=True, db_index=True)
+class Scraper(models.Model):
+    slug = models.SlugField()
+    schema = models.ForeignKey(Schema, related_name='scrapers')
 
 
-# class Stat(models.Model):
+class Run(models.Model):
+    scraper = models.ForeignKey(Scraper)
+    date = models.DateTimeField(auto_now_add=True, db_index=True)
