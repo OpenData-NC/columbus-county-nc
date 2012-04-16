6c8f1549c46d62b2c095e80d6b3a500bae2d8fd4
Colin Copeland <copelco@caktusgroup.com>
2012-04-16 12:28:05 -0400

    add breadcrumbs

--- openrural/data_dashboard/views.py
+++ openrural/data_dashboard/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Max
+from django.core.urlresolvers import reverse
+
+from ebpub.db import breadcrumbs
 
 from openrural.data_dashboard import models as dd
 
 
+def base_crumbs():
+    crumbs = breadcrumbs.home({})
+    crumbs.append(('Data Dashboard', reverse('dashboard')))
+    return crumbs
+
+
 def dashboard(request):
     scrapers = dd.Scraper.objects.annotate(last_run=Max('runs__end_date'))
-    context = {'scrapers': scrapers}
+    context = {'scrapers': scrapers, 'breadcrumbs': base_crumbs()}
     return render(request, 'data_dashboard/dashboard.html', context)
 
 
 def view_scraper(request, scraper_slug):
     scraper = get_object_or_404(dd.Scraper, slug=scraper_slug)
+    crumbs = base_crumbs()
+    crumbs.append((scraper_slug,
+                   reverse('view_scraper', args=[scraper_slug])))
     context = {'scraper': scraper,
-               'runs': scraper.runs.order_by('-end_date')}
+               'runs': scraper.runs.order_by('-end_date'),
+               'breadcrumbs': crumbs}
     return render(request, 'data_dashboard/view_scraper.html', context)
 
 
 def view_run(request, scraper_slug, run_id):
     run = get_object_or_404(dd.Run, scraper__slug=scraper_slug, pk=run_id)
-    context = {'run': run}
+    crumbs = base_crumbs()
+    crumbs.append((scraper_slug,
+                   reverse('view_scraper', args=[scraper_slug])))
+    crumbs.append(('Run %d' % run.pk,
+                   reverse('view_run', args=[scraper_slug, run_id])))
+    context = {'run': run,
+               'breadcrumbs': crumbs}
     return render(request, 'data_dashboard/view_run.html', context)
