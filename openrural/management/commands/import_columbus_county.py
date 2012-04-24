153785d23da683909cbbe7b1e7a4c718fbb086dc
Karen Tracey <kmtracey@caktusgroup.com>
2012-04-24 16:51:16 -0400

    Create a general County importer that uses 2010 Census data to import any US county with its contained places, townships, zips, blocks, etc. Use this importer (with a couple of overrides using county GIS data rather than census data) for the Columbus county import command.

--- openrural/management/commands/import_columbus_county.py
+++ openrural/management/commands/import_columbus_county.py
@@ -1,24 +1,15 @@
-import os
-import glob
-import tempfile
-
-from optparse import make_option, OptionParser
+from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
-from django.contrib.gis.geos import GEOSGeometry
 
-from ebpub.utils.text import slugify
 from ebpub.db.models import Location, LocationType
-from ebpub.db.bin.import_zips import ZipImporter
 from ebpub.db.bin.import_locations import LocationImporter
 from ebpub.metros.allmetros import get_metro
-from ebpub.utils.script_utils import die, makedirs, wget, unzip
-from ebpub.streets.blockimport.tiger.import_blocks import TigerImporter
-from ebpub.streets.bin import populate_streets
 
-STATE = '37' # NC
-COUNTY = '37047' # NC, Columbus county
+from openrural.management.commands.import_county import CountyImporter
+
+COLUMBUS_COUNTY = '37047' # NC, Columbus county
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -26,55 +17,32 @@ class Command(BaseCommand):
     )
     help = 'Import Columbus County data: county shape and townships, census places, blocks, zips'
 
-    def fetch_files(self, zip_dir):
-        orig_cwd = os.getcwd()
-        if zip_dir:
-            download = not os.path.exists(zip_dir)
-            if download:
-                os.makedirs(zip_dir)
-        else:
-            zip_dir = tempfile.mkdtemp()
-            download = True
-        os.chdir(zip_dir)
-        if download:
-            print 'Download TIGER & County GIS data to %s' % zip_dir
-            census_base_url = 'ftp://ftp2.census.gov/geo/tiger/TIGER2010'
-            county_base_url = 'http://www.columbusco.org/GISData'
-            census_zips = (
-                'PLACE/2010/tl_2010_%s_place10.zip' % STATE,
-                'EDGES/tl_2010_%s_edges.zip' % COUNTY,
-                'FACES/tl_2010_%s_faces.zip' % COUNTY,
-                'FEATNAMES/tl_2010_%s_featnames.zip' % COUNTY,
-                'ZCTA5/2010/tl_2010_%s_zcta510.zip' % STATE,
-                )
-            # Interestingly, versions of these files are available from the census data as well.
-            # However, at least the township one has clearly different boundaries for the townships.
-            # Going with the county data on the assumption it's coming from a source more likely to
-            # know what the "right" boundaries are.
-            county_zips = (
-                'County.zip',
-                'Township.zip',
-            )
-            for fname in census_zips:
-                fetch_name = '%s/%s' % (census_base_url, fname)
-                wget(fetch_name) or die(
-                    'Could not download %s' % fetch_name)
-            for fname in county_zips:
-                fetch_name = '%s/%s' % (county_base_url, fname)
-                wget(fetch_name) or die(
-                    'Could not download %s' % fetch_name)
-
-            for fname in glob.glob('*zip'):
-                unzip(fname) or die('Could not unzip %s' % fname)
-            print "Shapefiles unzipped in %s" % zip_dir
-        os.chdir(orig_cwd)
-        return zip_dir
-
     def handle(self, **options):
-        zip_dir = self.fetch_files(options['dir'])
 
+        county_importer = CountyImporter(COLUMBUS_COUNTY)
+
+        # Override a couple of the default files with county GIS versions.
+        county_importer.datafiles['county'] = {
+            'source': 'Columbus County GIS site',
+            'base_url': 'http://www.columbusco.org',
+            'path': 'GISData',
+            'file_name': 'County',
+            'name_field': 'NAME',
+        }
+        county_importer.datafiles['cousub'] = {
+            'source': 'Columbus County GIS site',
+            'base_url': 'http://www.columbusco.org',
+            'path': 'GISData',
+            'file_name': 'Township',
+            'name_field': 'NAME',
+        }
+
+        zip_dir = county_importer.fetch_files(options['dir'])
+
+        # Do our own import of the county location, using the Columbus County GIS file
+        # instead of the census NC Counties file. Columbus County file has just one
+        # feature in it.
         metro_name = get_metro()['metro_name'].upper()
-
         county_type_data = {
             'name': 'County',
             'plural_name': 'Counties',
@@ -89,79 +57,14 @@ class Command(BaseCommand):
             county_type = LocationType.objects.create(**county_type_data)
 
         Location.objects.filter(location_type=county_type).delete()
-        county_layer = DataSource('%s/County.shp' % zip_dir)[0]
+        county_layer = DataSource('%s/%s.shp' % (zip_dir, county_importer.datafiles['county']['file_name']))[0]
         loc_importer = LocationImporter(county_layer, county_type, filter_bounds=False, verbose=True)
-        loc_created_count = loc_importer.save('NAME')
-        # We are assuming here we are only going to have ONE county!
+        loc_created_count = loc_importer.save(county_importer.datafiles['county']['name_field'])
         columbus_county_location = Location.objects.get(location_type=county_type)
 
-        city_type_data = {
-            'name': 'City',
-            'plural_name': 'Cities',
-            'slug': 'cities',
-            'is_browsable': True,
-            'is_significant': True,
-            'scope': metro_name,
-        }
-        try:
-            city_type = LocationType.objects.get(slug=city_type_data['slug'])
-        except LocationType.DoesNotExist:
-            city_type = LocationType.objects.create(**city_type_data)
-
-        Location.objects.filter(location_type=city_type).delete()
-        city_layer = DataSource('%s/tl_2010_%s_place10.shp' % (zip_dir, STATE))[0]
-        loc_importer = LocationImporter(city_layer, city_type, filter_bounds=True, verbose=True)
-        loc_importer.bounds = columbus_county_location.location
-        loc_created_count = loc_importer.save('NAME10')
-
-        # Add in townships, deleting from their shapes any area already covered by a "proper" city.
-        starter_cities = Location.objects.filter(location_type=city_type)
-        within_cities = GEOSGeometry('MULTIPOLYGON EMPTY')
-        for city in starter_cities:
-            within_cities = within_cities.union(city.location)
-        city_pks = [l.pk for l in starter_cities]
-        township_layer = DataSource('%s/Township.shp' % zip_dir)[0]
-        loc_importer = LocationImporter(township_layer, city_type, filter_bounds=False, verbose=True)
-        loc_created_count = loc_importer.save('NAME')
-        townships = Location.objects.filter(location_type=city_type).exclude(pk__in=city_pks)
-        for township in townships:
-            township.name = '%s environs' % township.name.title()
-            township.slug = slugify(township.name)
-            township.location = township.location.difference(within_cities)
-            township.save()
-
-        # Zipcodes are used by the block importer
-        Location.objects.filter(location_type__slug='zipcodes').delete()
-        zip_layer = DataSource('%s/tl_2010_%s_zcta510.shp' % (zip_dir, STATE))[0]
-        zip_importer = ZipImporter(zip_layer, name_field='ZCTA5CE10', source='2010 Tiger/Census',
-            filter_bounds=True, verbose=True)
-        zip_importer.bounds = columbus_county_location.location
-        loc_created_count = zip_importer.save()
-
-        # Now we load them the blocks table.
-        print "Importing blocks, this may take several minutes ..."
-
-        importer = TigerImporter(
-            '%s/tl_2010_%s_edges.shp' % (zip_dir, COUNTY),
-            '%s/tl_2010_%s_featnames.dbf' % (zip_dir, COUNTY),
-            '%s/tl_2010_%s_faces.dbf' % (zip_dir, COUNTY),
-            '%s/tl_2010_%s_place10.shp' % (zip_dir, STATE),
-            fix_cities=True,
-            encoding='utf8',
-            filter_bounds=columbus_county_location.location)
-        num_created = importer.save()
-        print "Created %d blocks" % num_created
-
-        #########################
-
-        print "Populating streets and fixing addresses, these can take several minutes..."
-
-        # Note these scripts should be run ONCE, in this order,
-        # after you have imported *all* your blocks.
-
-        populate_streets.main(['streets'])
-        populate_streets.main(['block_intersections'])
-        populate_streets.main(['intersections'])
+        # Set the county location in the county_importer and call its full_import to do the rest.
+        county_importer.county_location = columbus_county_location
+        county_importer.full_import()
         print "Done."
 
         if not options['dir']:
---
+++ openrural/management/commands/import_county.py
@@ -0,0 +1,243 @@
+import os
+import glob
+import tempfile
+import datetime
+
+from optparse import make_option, OptionParser
+
+from django.core.management.base import BaseCommand
+from django.contrib.gis.gdal import DataSource
+from django.contrib.gis.geos import GEOSGeometry
+
+from ebpub.utils.text import slugify
+from ebpub.db.models import Location, LocationType
+from ebpub.db.bin.import_zips import ZipImporter
+from ebpub.db.bin.import_locations import LocationImporter
+from ebpub.metros.allmetros import get_metro
+from ebpub.utils.geodjango import ensure_valid
+from ebpub.utils.geodjango import flatten_geomcollection
+from ebpub.utils.script_utils import die, makedirs, wget, unzip
+from ebpub.streets.blockimport.tiger.import_blocks import TigerImporter
+from ebpub.streets.bin import populate_streets
+from ebpub.geocoder.parser.parsing import normalize
+
+
+def census_name_for(shapetype, state, county):
+    start = 'tl_2010'
+    end = '10'
+    if shapetype in ('edges', 'faces', 'featnames'):
+        return '%s_%s_%s' % (start, county, shapetype)
+    elif shapetype in ('place', 'county'):
+        return '%s_%s_%s%s' % (start, state, shapetype, end)
+    elif shapetype == 'cousub':
+        return '%s_%s_%s%s' % (start, county, shapetype, end)
+    elif shapetype == 'zips':
+        return '%s_%s_zcta5%s' % (start, state, end)
+
+
+class CountyImporter(object):
+    def __init__(self, county, county_location=None):
+        self.county = county
+        state = self.state = county[0:2]
+        self.county_location = None
+        self.metro_name = get_metro()['metro_name'].upper()
+
+        self.datafiles = {}
+        self.datafiles['zips'] = {
+            'path': 'ZCTA5/2010',
+            'name_field': 'ZCTA5CE10',
+        }
+        for key in ('county', 'cousub', 'place'):
+            self.datafiles[key] = {
+                'path': '%s/2010' % key.upper(),
+                'name_field': 'NAME10',
+            }
+        for key in ('edges', 'faces', 'featnames'):
+            self.datafiles[key] = {
+                'path': key.upper(),
+            }
+        for key in self.datafiles.keys():
+            self.datafiles[key]['file_name'] = census_name_for(key, state, county)
+            self.datafiles[key]['base_url'] = 'ftp://ftp2.census.gov/geo/tiger/TIGER2010'
+            self.datafiles[key]['source'] = '2010 Tiger Census data'
+
+    def fetch_files(self, zip_dir):
+        orig_cwd = os.getcwd()
+        if zip_dir:
+            download = not os.path.exists(zip_dir)
+            if download:
+                os.makedirs(zip_dir)
+        else:
+            zip_dir = tempfile.mkdtemp()
+            download = True
+        self.zip_dir = zip_dir
+        os.chdir(zip_dir)
+        if download:
+            print 'Download data files to %s' % zip_dir
+            for k, v in self.datafiles.items():
+                fetch_name = '%s/%s/%s.zip' % (v['base_url'], v['path'], v['file_name'])
+                wget(fetch_name) or die('Could not download %s' % fetch_name)
+            for fname in glob.glob('*zip'):
+                unzip(fname) or die('Could not unzip %s' % fname)
+            print "Shapefiles unzipped in %s" % zip_dir
+        os.chdir(orig_cwd)
+        return zip_dir
+
+    def import_county(self):
+        county_type_data = {
+            'name': 'County',
+            'plural_name': 'Counties',
+            'slug': 'counties',
+            'is_browsable': True,
+            'is_significant': True,
+            'scope': self.metro_name,
+        }
+        try:
+            county_type = LocationType.objects.get(slug=county_type_data['slug'])
+        except LocationType.DoesNotExist:
+            county_type = LocationType.objects.create(**county_type_data)
+
+        Location.objects.filter(location_type=county_type).delete()
+        county_layer = DataSource('%s/%s.shp' % (self.zip_dir,
+            self.datafiles['county']['file_name']))[0]
+        now = datetime.datetime.now()
+        county_location = None
+        for feature in county_layer:
+            if feature.get('GEOID10') == self.county:
+                name = feature.get(self.datafiles['county']['name_field'])
+                geom = feature.geom.transform(4326, True).geos
+                geom = ensure_valid(geom, name)
+                geom = flatten_geomcollection(geom)
+                loc_fields = dict(
+                    name = name,
+                    slug = slugify(name),
+                    location_type = county_type,
+                    location = geom,
+                    city = self.metro_name,
+                    is_public = True,
+                )
+                kwargs = dict(
+                    loc_fields,
+                )
+                kwargs.update({
+                    'creation_date': now,
+                    'last_mod_date': now,
+                    'display_order': 0,
+                    'normalized_name': normalize(loc_fields['name']),
+                    'area': loc_fields['location'].transform(3395, True).area,
+                })
+                county_location = Location.objects.create(**kwargs)
+                break
+        return county_location
+
+    def import_starter_cities(self):
+        fkey = 'place'
+        type_data = {
+            'name': 'City',
+            'plural_name': 'Cities',
+            'slug': 'cities',
+            'is_browsable': True,
+            'is_significant': True,
+            'scope': self.metro_name,
+        }
+        try:
+            self.city_type = LocationType.objects.get(slug=type_data['slug'])
+        except LocationType.DoesNotExist:
+            self.city_type = LocationType.objects.create(**type_data)
+
+        Location.objects.filter(location_type=self.city_type).delete()
+        layer = DataSource('%s/%s.shp' % (self.zip_dir, self.datafiles[fkey]['file_name']))[0]
+        loc_importer = LocationImporter(layer,
+            self.city_type,
+            source = self.datafiles[fkey].get('source', 'Unknown'),
+            filter_bounds=True,
+            verbose=True)
+        loc_importer.bounds = self.county_location.location
+        loc_created_count = loc_importer.save(self.datafiles[fkey]['name_field'])
+        return loc_created_count
+
+    def augment_cities(self):
+        # Add in county subdivisions, deleting from their shapes any area already covered by a "proper" city.
+        fkey = 'cousub'
+        starter_cities = Location.objects.filter(location_type=self.city_type)
+        within_cities = GEOSGeometry('MULTIPOLYGON EMPTY')
+        for city in starter_cities:
+            within_cities = within_cities.union(city.location)
+        city_pks = [l.pk for l in starter_cities]
+        layer = DataSource('%s/%s.shp' % (self.zip_dir, self.datafiles[fkey]['file_name']))[0]
+        loc_importer = LocationImporter(layer,
+            self.city_type,
+            source = self.datafiles[fkey].get('source', 'Unknown'),
+            filter_bounds=False,
+            verbose=True)
+        loc_created_count = loc_importer.save(self.datafiles[fkey]['name_field'])
+        townships = Location.objects.filter(location_type=self.city_type).exclude(pk__in=city_pks)
+        for township in townships:
+            township.name = '%s environs' % township.name.title()
+            township.slug = slugify(township.name)
+            township.location = township.location.difference(within_cities)
+            township.save()
+        return loc_created_count
+
+    def import_zips(self):
+        fkey = 'zips'
+        Location.objects.filter(location_type__slug='zipcodes').delete()
+        zip_layer = DataSource('%s/%s.shp' % (self.zip_dir, self.datafiles[fkey]['file_name']))[0]
+        zip_importer = ZipImporter(zip_layer,
+            name_field=self.datafiles[fkey]['name_field'],
+            source=self.datafiles[fkey].get('source', 'Unknown'),
+            filter_bounds=True,
+            verbose=True)
+        zip_importer.bounds = self.county_location.location
+        loc_created_count = zip_importer.save()
+        return loc_created_count
+
+    def import_blocks(self):
+        # Now we load them the blocks table.
+        print "Importing blocks, this may take several minutes ..."
+
+        importer = TigerImporter(
+            '%s/%s.shp' % (self.zip_dir, self.datafiles['edges']['file_name']),
+            '%s/%s.dbf' % (self.zip_dir, self.datafiles['featnames']['file_name']),
+            '%s/%s.dbf' % (self.zip_dir, self.datafiles['faces']['file_name']),
+            '%s/%s.shp' % (self.zip_dir, self.datafiles['place']['file_name']),
+            fix_cities=True,
+            encoding='utf8',
+            filter_bounds=self.county_location.location)
+        num_created = importer.save()
+        print "Created %d blocks" % num_created
+
+        #########################
+
+        print "Populating streets and fixing addresses, these can take several minutes..."
+
+        # Note these scripts should be run ONCE, in this order,
+        # after you have imported *all* your blocks.
+
+        populate_streets.main(['streets'])
+        populate_streets.main(['block_intersections'])
+        populate_streets.main(['intersections'])
+
+    def full_import(self):
+        if not self.county_location:
+            self.county_location = self.import_county()
+        self.import_starter_cities()
+        self.augment_cities()
+        self.import_zips()
+        self.import_blocks()
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option("-d", "--dir", action="store", type="string", dest="dir"),
+    )
+    help = 'Import county data: county shape, places and county subdivisions, blocks and zips'
+
+    def handle(self, county, **options):
+        county_importer = CountyImporter(county)
+        zip_dir = county_importer.fetch_files(options['dir'])
+        county_importer.full_import()
+        print "Done."
+
+        if not options['dir']:
+            print "Removing temp directory %s" % zip_dir
+            os.system('rm -rf %s' % zip_dir)
