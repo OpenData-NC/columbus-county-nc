a8360d3d26917a1cbb3f6e560552faf4e2a1b703
Colin Copeland <copelco@caktusgroup.com>
2012-06-12 17:26:27 -0400

    add settings for sandbox

---
+++ openrural/settings_sandbox.py
@@ -0,0 +1,14 @@
+from openrural.settings import *
+
+# Graylog2 logging handler
+LOGGING['handlers']['gelf'] = {
+    'class': 'openrural.data_dashboard.handlers.CustomGELFHandler',
+    'host': 'monitor2.caktusgroup.com',
+    'port': 12201,
+    'extra_fields': {
+        'deployment': 'columbusco',
+        'environment': 'unknown', # overridden in local_settings.py
+    },
+}
+LOGGING['loggers'][''] = {'handlers': ['gelf'], 'level': 'DEBUG',
+                          'propagate': True}
