5b02a2e83854a3de4ffc25223c2df1e9a7d11636
juliaelman <julia.elman@gmail.com>
2012-06-29 10:37:59 -0400

    Adding a templatetag for grabbing public schemas. Also added a list of public schemas to the homepage and removed the times news items.

---
+++ openrural/templatetags/openrural_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+from ebpub.db.models import Schema
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def get_schema_list(context):
+    schemas = Schema.objects.filter(is_public=True).values('name', 'url', 'slug')
+
+    context["schema_list"] = schemas
+    return ''
