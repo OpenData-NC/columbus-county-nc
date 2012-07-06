05db93644e2f4e44efbee0f801da70a4acb3199d
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-07-06 09:12:03 -0400

    refs #66 - Render cities and areas in alphabetical order

--- openrural/templatetags/openrural_tags.py
+++ openrural/templatetags/openrural_tags.py
@@ -9,3 +9,8 @@ def get_schema_list(context):
     schemas = Schema.objects.filter(is_public=True)
     context["schema_list"] = schemas
     return ''
+
+@register.simple_tag(takes_context=True)
+def reorder_cities_list(context):
+    context['location_list'] = context['location_list'].order_by('name')
+    return ''
