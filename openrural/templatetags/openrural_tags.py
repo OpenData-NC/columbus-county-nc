3100c0f6888de0d7e7f2c533206c992186f26fe7
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-07-09 16:47:14 -0400

    Work on #83

--- openrural/templatetags/openrural_tags.py
+++ openrural/templatetags/openrural_tags.py
@@ -14,3 +14,18 @@ def get_schema_list(context):
 def reorder_cities_list(context):
     context['location_list'] = context['location_list'].order_by('name')
     return ''
+
+@register.simple_tag
+def get_corporation_full_address(newsitem):
+    addr = newsitem.location_name.strip()
+    locs = newsitem.location_set.all()
+    try:
+        city = locs.filter(location_type__slug='cities').get().name.strip()
+    except:
+        city = ''
+    try:
+        zip_code = locs.filter(location_type__slug='zipcodes').get().name.strip()
+    except:
+        zip_code = ''
+
+    return '{0}, {1}, NC {2}'.format(addr, city, zip_code).strip()
